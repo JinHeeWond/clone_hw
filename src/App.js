84444c69c8693a5229669fef5c3be8021e5cc854
JinHeeWond <jin2749708@handong.ac.kr>
2025-03-26 12:31:22 +0900

    appjs수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, { useState } from "react";
 import Header from "./components/Header";
 import Dashboard from "./components/Dashboard";
-import styled from "styled-components";
 
-const Layout = styled.div`
-  display: flex;
-  margin-top:100px;
-  `;
 
 function App() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
+ 
   return (
     <div>
-      <Header toggleSidebar={toggleSidebar} />
+      <Header/>
         <Dashboard />
     </div>
   );
